4e459a035bf39cc89355b03137e3d480bb5750b3
Henry Rios <delzep@gmail.com>
2021-09-20 15:46:32 -0500

    Change:tutorial from reactjs.org

--- src/App.js
+++ src/App.js
@@ -4,14 +4,16 @@ import Header from "./components/Header"
 import MainContent from "./components/MainContent"
 import Footer from "./components/Footer"
 
+import './App.css';
+
 function App() {
-    return (
-        <div>
-            <Header />
-            <MainContent />
-            <Footer />
-        </div>
-    )
+  return (
+    <div className="App">
+      <Header />
+      <MainContent />
+      <Footer />
+    </div>
+  );
 }
 
-export default App
+export default App;
