4f1d9746b0c4358b681a33ad3ad35020a515fb34
Henry Rios <delzep@gmail.com>
2021-09-22 13:49:39 -0500

    Add: external data sample (json file)

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -1,29 +1,15 @@
 import React from "react";
 import ContactCard from "./cards/ContactCard";
+import Contacts from "../data/contacts.json"
 
 function MainContent() {
-    // sample data
-    const contacts = [
-        {
-            name: 'Mr. first', 
-            imgUrl: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse3.mm.bing.net%2Fth%3Fid%3DOIP.tOY8AMzEt0iNMNATuYh2agAAAA%26pid%3DApi&f=1',
-            phone: '+57 325 654321',
-            email: 'first@site.com'
-        }, 
-        {
-            name: 'Mr. second', 
-            imgUrl: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.UxRMdncPXSsWj3WZlutmmwAAAA%26pid%3DApi&f=1',
-            phone: '+57 300 321987',
-            email: 'second@site.com'
-        }
-    ];
     // map data for contactList
-    const contactList = contacts.map((contact, i) => {
-                    return (<ContactCard key={i} contact={contact} index={i} />);
-                });
+    const contactList = Contacts.map(contact => <ContactCard key={contact.id} contact={contact} />);
     // return content
     return (
-        <main>{contactList}</main>
+        <main>
+            {contactList}
+        </main>
     );
 }
 
--- src/components/cards/ContactCard.js
+++ src/components/cards/ContactCard.js
@@ -5,7 +5,7 @@ function ContactCard(props) {
     return (
         <div className="contact-card">
             <img src={props.contact.imgUrl} alt={props.contact.name} />
-            <h3>{props.index + 1} - {props.contact.name}</h3>
+            <h3>{props.contact.id} - {props.contact.name}</h3>
             <p>Phone: {props.contact.phone}</p>
             <p>Email: {props.contact.email}</p>
         </div>
